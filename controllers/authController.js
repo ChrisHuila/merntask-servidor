03daeefe2b182c0529f3bf8e777199fc0343cd1b
chrisHuila <christian.huila22@gmail.com>
2023-04-16 17:05:23 -0500

    funcion agregada a authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,7 +47,7 @@ exports.autenticarUsuario = async (req, resp) => {
 // Obtiene el usuario autenticado
 exports.usuarioAutenticado = async (req, res) => {
     try {
-        const usuario = await Usuario.findById(req.usuario)
+        const usuario = await Usuario.findById(req.usuario.id).select('-password')
         res.json({usuario})
     } catch (error) {
         console.log(error);
