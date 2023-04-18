37c468173e719ec4552d0b5df92b255d5913f931
chrisHuila <christian.huila22@gmail.com>
2023-04-18 16:12:58 -0500

    puerto modificado

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, "0.0.0.0/0" ,() =>{
-    console.log(" el servidor funciana en el puerto " + PORT );
+app.listen(port, "0.0.0.0" ,() =>{
+    console.log(" el servidor funciana en el puerto " + port );
 })
